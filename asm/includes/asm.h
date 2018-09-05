75d92bb19e5567176c5a7a004daa82d8ad6b1428
Zack BATIK <zbatik@e4r21p2.wethinkcode.co.za>
2018-09-05 10:33:11 +0200

    added include libft and a t_byte typedef to asm.h

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -3,16 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   asm.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdilapi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 13:29:35 by mdilapi           #+#    #+#             */
-/*   Updated: 2018/09/04 13:29:36 by mdilapi          ###   ########.fr       */
+/*   Updated: 2018/09/05 10:31:55 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_H
 # define ASM_H
-# include <libft.h>
+
+#include "../libft/includes/libft.h"
+
+typedef unsigned char	t_byte;
 
 typedef struct	s_instruct
 {
@@ -26,12 +29,18 @@ typedef struct	s_instruct
 
 }				t_instruct;
 
+/*
+** lable list can be replaced with a libft t_list
+*/
+
+/*
 typedef struct	s_label
 {
 	char	*name;
 	int		index;
 	
 }				t_label;
+*/
 #endif
 
 /**
