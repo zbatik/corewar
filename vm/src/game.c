cca1af4bcf025c60469e3dd0d59ef398114e0aa0
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-09-09 16:38:33 +0200

    fixed game logic: still haven't compiled with it

--- vm/src/game.c
+++ vm/src/game.c
@@ -6,7 +6,7 @@
 /*   By: zbatik <zbatik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/09 15:43:52 by zbatik            #+#    #+#             */
-/*   Updated: 2018/09/09 16:27:53 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/09/09 16:36:40 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,15 @@ static int update_cycles_to_execute(t_byte current, t_process *cursor)
 static int execute_pcs(t_core *core)
 {
     int i;
+    int move;
 
     i = -1;
-    while (++i < num_processes)
-        execute_pc(&core->num_processes[i]);
+    while (++i < core->num_processes)
+    {
+        move = execute_pc(&core->processes[i]);
+        core->cursor[core->processes[i].pc - move] = 0;
+        core->cursor[core->processes[i].pc] = 1;
+    }
     return (1);
 }
 
@@ -44,16 +49,22 @@ static int execute_pc(t_process *cursor, t_byte *mem)
         cursor->pc = (cursor->pc + move) % MEM_SIZE;
         update_cycles_to_execute(mem[cursor->pc], cursor);
     }
+    else
+        move = 0;
+    return (move);
 }
 
 int game_loop(t_core *core)
 {
     int i;
+    char *line;
 
     i = -1;
     while (++i < core->cycles_to_die)
     {
-        execute_pcs(t_core *core)
+        get_next_line(0, &line);
+        ft_strdel(&line);
+        execute_pcs(core);
         core->cycles_to_die -= CYCLE_DELTA;
     }
 }
