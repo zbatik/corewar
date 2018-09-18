067ab467f850be7a27547fd2b27bdd1013dced87
zbatk <zbatik@student.wethinkcode.co.za>
2018-09-18 21:08:30 +0200

    added allcombinations of all instruction to test and a script to run them

---
+++ asm/test_instructions/and.s
@@ -0,0 +1,6 @@
+and r1,r2,r3	# RRR
+and r1,42,r3	# RIR
+and %10,r2,r3	# IRR
+and 11,12,r3	# IIR
+and %13,r2,r3	# DRR
+and %14,15,r3	# DIR
---
+++ asm/test_instructions/xor.s
@@ -0,0 +1,6 @@
+xor r1,r2,r3	# RRR
+xor r1,42,r3	# RIR
+xor %10,r2,r3	# IRR
+xor 11,12,r3	# IIR
+xor %13,r2,r3	# DRR
+xor %14,15,r3	# DIR
