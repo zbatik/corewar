21fb765a210be031bc6f6285d0d60e9052f5a66f
Fred-Dee <fred.dilapisho@gmail.com>
2018-09-24 16:45:32 +0200

    as far as possible normed the shared files

--- shared/includes/shared.h
+++ shared/includes/shared.h
@@ -6,62 +6,61 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 10:40:04 by zbatik            #+#    #+#             */
-/*   Updated: 2018/09/06 18:58:46 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/09/24 16:34:00 by mdilapi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHARED_H
 # define SHARED_H
 
-#include "../libft/includes/libft.h"
-#include "op.h"
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-
+# include "../libft/includes/libft.h"
+# include "op.h"
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
 
 # define TRUE 1
 # define FALSE 0
 
-# define RRR 0x54 // 0b01010100 //checked with and
-# define RIR 0x74 // 0b01110100 //checked with and
-# define RDR 0x64 // 0b01100100
-# define RRD 0x58 // 0b10111000 // changed from B8 due to sti
-# define RDD 0x68 // first example sti
-# define RID 0x78 // first example sti
-# define IRR 0xD4 // 0b11010100
-# define IIR 0xF4 // 0b11110100
-# define IDR 0xE4 // 0b11100100
-# define DRR 0x94 // 0b10010100 //checked with and
-# define DIR 0xB4 // 0b10110100 //checked with and
-# define DDR 0xA4 // 0b10100100
-# define IR  0xD0 // 0b11010000 // checked with ld
-# define DR  0x90 // 0b10010000 // checked with ld
-# define RI  0x70 // checked with st
-# define RR  0x50 // checked with st
+# define RRR 0x54
+# define RIR 0x74
+# define RDR 0x64
+# define RRD 0x58
+# define RDD 0x68
+# define RID 0x78
+# define IRR 0xD4
+# define IIR 0xF4
+# define IDR 0xE4
+# define DRR 0x94
+# define DIR 0xB4
+# define DDR 0xA4
+# define IR  0xD0
+# define DR  0x90
+# define RI  0x70
+# define RR  0x50
 # define R   0x40
 
-# define B_RRR 5    // checked with ldi
-# define B_RIR 6	// checked with sti
-# define B_RDR 8	// checked with ldi
-# define B_RRD 8	// checked with sti	
-# define B_RDD 11	// checked with sti
-# define B_RID 9	// checked with sti
-# define B_IRR 6	// checked with ldi
-# define B_IIR 7 // WRONG PLEASE CHANGE!!!
-# define B_IDR 9	// checked with ldi
-# define B_DRR 8  	// checked with ldi
-# define B_DIR 9    // checked with and
+# define B_RRR 5
+# define B_RIR 6
+# define B_RDR 8
+# define B_RRD 8
+# define B_RDD 11
+# define B_RID 9
+# define B_IRR 6
+# define B_IIR 7
+# define B_IDR 9
+# define B_DRR 8
+# define B_DIR 9
 # define B_DDR 11
-# define B_IR  5	// checked with ldi
-# define B_DR  7	// checked with ldi
-# define B_RI  5	// checked with st
-# define B_RR  4	// checked with
+# define B_IR  5
+# define B_DR  7
+# define B_RI  5
+# define B_RR  4
 # define B_R   3
 
 typedef unsigned char	t_byte;
-	
+
 typedef enum {
 	e_null,
 	e_live,
@@ -80,7 +79,7 @@ typedef enum {
 	e_lldi,
 	e_lfork,
 	e_aff,
-} t_opnum;
+}	t_opnum;
 
 typedef struct	s_opinfo
 {
@@ -90,11 +89,10 @@ typedef struct	s_opinfo
 	short		op_number;
 	int			cycles;
 	char		description[128];
-	t_bool		modifies_carry;		/* could be wrong */
-	t_bool		funky_dir_size;   		/* could be wrong */
+	t_bool		modifies_carry;
+	t_bool		funky_dir_size;
 }				t_opinfo;
 
-t_opinfo	index_opinfo(t_opnum op_index);
-unsigned int rev_endian(unsigned int num);
-
+t_opinfo		index_opinfo(t_opnum op_index);
+unsigned int	rev_endian(unsigned int num);
 #endif
